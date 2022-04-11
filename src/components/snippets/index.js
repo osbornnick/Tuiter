953474d007674e847a968d5d89b809e4667790d4
Yanan-G <yanangeng@Yanans-Mac-mini.local>
2022-04-11 13:53:37 -0400

    #15 barebone snippets component

--- src/components/snippets/index.js
+++ src/components/snippets/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { oneDark } from '@codemirror/theme-one-dark';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+
+
 const Snippets = () => {
+  const [output, setOutput] = useState("");
+
+  let code = "console.log('hello world!');"
+
+  const saveCode = () => {
+    // connect to backend
+  }
+  const runCode = () => {
+    // compiler API
+  };
+
   return(
-    <h1>My Snippets Screen</h1>
+    <>
+      <CodeMirror
+        value={code}
+        height="400px"
+        theme={oneDark}
+        extensions={[javascript({ jsx: true })]}
+        onChange={(value, viewUpdate) => (code = value)}
+      />
+        <button onClick={runCode}>Run</button>
+        <button onClick={saveCode}>Save</button>
+    </>
   );
 };
 export default Snippets;
