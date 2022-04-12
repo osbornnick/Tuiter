85068d8ce6e0aed5d1ecdd19918027fe96020952
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-04-12 11:28:01 -0400

    snippets should display a list of snippet items

--- src/components/snippets/index.js
+++ src/components/snippets/index.js
@@ -1,39 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { oneDark } from '@codemirror/theme-one-dark';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-import './snippets.css'
-
+import snippet_data from "./test_snippets.json";
+import Snippet from "./snippet";
 
 const Snippets = () => {
-  const [output, setOutput] = useState("");
-
-  let code = "console.log('hello world!');"
-
-  const saveCode = () => {
-    // connect to backend
-  }
-  const runCode = () => {
-    // compiler API
-  };
-
-  return(
-    <>
-      <CodeMirror
-        value={code}
-        height="400px"
-        theme={oneDark}
-        extensions={[javascript({ jsx: true })]}
-        onChange={(value, viewUpdate) => (code = value)}
-      />
-      <div class="row">
-        <button className="run-btn" onClick={runCode}>Run</button>
-        <button className="save-btn" onClick={saveCode}>Save</button>
-      </div>      
-      <div className="output-box">
-        Output
-      </div>
-    </>
-  );
+    return (
+        <div className="container">
+            {snippet_data.map((snip) => (
+                <div className="row mb-4">
+                    <Snippet snippet={snip} />
+                </div>
+            ))}
+        </div>
+    );
 };
 export default Snippets;
